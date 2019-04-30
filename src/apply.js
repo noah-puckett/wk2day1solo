a1bd2ff509a2a4a75d8cfcc0674e8cfa05a5cba2
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-04-29 21:35:54 -0700

    wired up forum event listener template

---
+++ src/apply.js
@@ -0,0 +1,7 @@
+//get forum element
+const form = document.getElementById('application');
+
+//subscribe event listener/wire up submit
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+});
--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,17 @@
-
+//TODO: IMPORT CREATED FUNCTION
 const test = QUnit.test;
+
+const function = possumArmyObj(FormData) {
+
+}
+
 
 test('time to test a function', function(assert) {
     //Arrange
+
     // Set up your parameters and expectations
+    //create an INSTANCE (object) of built-in FormData CLASS.
+    const form = new FormData();
 
     //Act 
     // Call the function you're testing and set the result to a const
